990535f7b9f4c1179ef492b3d825f0943de91784
Shavej-Malik <shavejsbi@gmail.com>
2026-08-18 17:56:10 +0530

    More on DOM

---
+++ Day-44 More on DOM/script.js
@@ -0,0 +1,17 @@
+//create element
+//append 
+
+// var h1 = document.createElement("h1")
+// console.log(h1)
+
+var btn = document.querySelector("button")
+var main = document.querySelector("main")
+// btn.addEventListener("click",function(){
+//     var h1 = document.createElement("h1")
+//     h1.innerHTML="Hello Friends"
+//     console.log(h1)
+// })
+var h1 = document.createElement("h1")
+    h1.innerHTML="Hello Friends"
+main.appendChild(h1)
+
