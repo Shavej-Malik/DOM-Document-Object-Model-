5340e9634843e4fbbdb2e7682816b6ac824d6a10
Shavej-Malik <shavejsbi@gmail.com>
2026-08-22 18:46:24 +0530

    Different Event listener in DOM

---
+++ DAy-48 Different EventListener in DOM/scrupt.js
@@ -0,0 +1,25 @@
+var img = document.querySelector("img")
+var msg = document.querySelector("h2 span")
+var body = document.body
+// img.addEventListener("click", function(){
+//     msg.textContent = "Undertood!"
+//     body.style.backgroundColor = "orange"
+// })
+
+// img.addEventListener("dblclick", function(){
+//     msg.textContent = "Undertood!"
+//     body.style.backgroundColor = "orange"
+// })
+
+// img.addEventListener("mouseenter", function(){
+//     msg.textContent = "Undertood!"
+//     body.style.backgroundColor = "orange"
+// })
+// img.addEventListener("mouseleave", function(){
+//     msg.textContent = ""
+//     body.style.backgroundColor = ""
+// })
+
+// img.addEventListener("mousemove", function(){
+//     console.log("Hello")
+// })
